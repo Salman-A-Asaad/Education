4c844f1e17baeeb2d2cb2f1cfac9ca6353b8f100
Salman-A-Asaad <salman.asaad.31.5.2000@gmail.com>
2023-09-11 17:39:19 +0300

    Add href for site and fix some bugs

---
+++ README.md
@@ -0,0 +1,7 @@
+# Education Website
+
+This site provides an interface that could be specific to one of the educational centers, schools, or universities that offer courses for all those interested in fields related to education.
+
+## Website
+
+[Demo](https://salman-a-asaad.github.io/Education/)
--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let bars = document.getElementById("bars");
 let list = document.getElementById("list");
 let teachers = document.querySelectorAll(".teachers .container .item ");
+let year = document.getElementById("year");
+year.innerHTML = `${new Date().getFullYear()}`;
 let comunication = document.querySelectorAll(
   ".teachers .container .item .comunication"
 );
