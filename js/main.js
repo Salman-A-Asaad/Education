4c610f2d96f4118b23f9f7f8ddcd46597b1142fc
Salman-A-Asaad <salman.asaad.31.5.2000@gmail.com>
2023-09-12 13:52:14 +0300

    Add slider to course section

--- js/main.js
+++ js/main.js
@@ -2,10 +2,14 @@ let bars = document.getElementById("bars");
 let list = document.getElementById("list");
 let teachers = document.querySelectorAll(".teachers .container .item ");
 let year = document.getElementById("year");
-year.innerHTML = `${new Date().getFullYear()}`;
 let comunication = document.querySelectorAll(
   ".teachers .container .item .comunication"
 );
+let itemsCourse = document.querySelector(".courses .container .items");
+let itemsCourseOne = document.getElementById("item-one");
+let next = document.getElementById("next");
+let pre = document.getElementById("pre");
+year.innerHTML = `${new Date().getFullYear()}`;
 teachers.forEach((element, index) => {
   element.addEventListener("click", () => {
     comunication[index].classList.toggle("show");
@@ -14,3 +18,12 @@ teachers.forEach((element, index) => {
 bars.addEventListener("click", () => {
   list.classList.toggle("show");
 });
+
+next.onclick = () => {
+  let widthItem = parseFloat(window.getComputedStyle(itemsCourseOne).width);
+  itemsCourse.scrollLeft += widthItem + 33;
+};
+pre.onclick = () => {
+  let widthItem = parseFloat(window.getComputedStyle(itemsCourseOne).width);
+  itemsCourse.scrollLeft -= widthItem + 33;
+};
